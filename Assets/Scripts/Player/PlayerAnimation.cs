4f63d3a9f013ecf404922ebc6d6a33684204abd5
LSEANL03 <haov80530@gmail.com>
2023-03-04 23:29:46 +0700

    update

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb2d;
+    public enum Movementstate { Idle, Running, Jumping, Falling }
+    private void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.rb2d= GetComponent<Rigidbody2D>();
+    }
+    public void AnimationPlayer(float directionX)
+    {
+        Movementstate state;
+
+        if (directionX > 0f)
+        {
+            state = Movementstate.Running;
+        }
+        else if (directionX < 0f)
+        {
+            state = Movementstate.Running;
+        }
+        else
+        {
+            state = Movementstate.Idle;
+        }
+        if (this.rb2d.velocity.y > .1f)
+        {
+            state = Movementstate.Jumping;
+        }
+        else if (this.rb2d.velocity.y < -.1f)
+        {
+            state = Movementstate.Falling;
+        }
+        animator.SetInteger("State", (int)state);//cài đặt số nguyên
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{  
+    private Rigidbody2D rb2d;
+    private BoxCollider2D coll; //cuộn dây
+    public PlayerAnimation playerAnimation;
+    [SerializeField] private float directionX=0f; //phương hướng
+    [SerializeField] private float speedMove = 3f;
+    [SerializeField] private float jumpForce = 10.5f;//lực nhảy
+    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private bool facingRight = true;
+    public bool canMove=true;
+    private void Start()
+    {
+        this.playerAnimation = GetComponent<PlayerAnimation>();
+        this.rb2d = GetComponent<Rigidbody2D>();
+        this.coll= GetComponent<BoxCollider2D>();
+    }
+    private void Update()
+    {
+        if (canMove)
+        {
+            this.MovePlayer();
+            this.playerAnimation.AnimationPlayer(directionX);
+            this.PLayerFlip();
+        }
+    }
+    void MovePlayer()
+    {
+        this.directionX = Input.GetAxisRaw("Horizontal");
+        this.rb2d.velocity = new Vector2(this.directionX * this.speedMove, this.rb2d.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space)&&IsGrounded())
+        {
+            this.rb2d.velocity = new Vector2(this.rb2d.velocity.x, this.jumpForce);
+            //FindObjectOfType<AudioManager>().PlaySounds("Jump");
+        }
+    }
+    void PLayerFlip()
+    {
+        if(this.directionX > 0f && !facingRight)
+            this.Flip();
+        else if(this.directionX<0f && facingRight)
+            this.Flip();
+    }
+    void Flip()
+    {
+        facingRight=!facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);//giới hạn
+    }
+
+}
+
+
+
+
+
